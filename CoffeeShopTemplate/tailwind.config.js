79f4c67bf0b57c65f74376a4619e314575472216
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2024-08-18 20:22:17 -0700

    wt-coffee

--- CoffeeShopTemplate/tailwind.config.js
+++ CoffeeShopTemplate/tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "brown" : {
+          "100" :  "#ECE0D1",
+          "300" :  "#DBC1AC",
+          "600" :  "#967259",
+          "900" :  "#634832",
+        }
+      },
+      boxShadow : {
+        "shadow-normal" : "0px 1px 10px 0px #0000000D",
+      },
+      borderRadius : {
+        "4xl" : "2rem",
+      },
+      fontFamily : {
+        "DanaBld" : "Dana Bold",
+        "DanaMd" : "Dana Medium",
+        "DanaRegu" : "Dana Regular",
+        "squarBld" : "squar Bold",
+        "squarMd" : "squar Medium",
+        "squarRegu" : "squar Light",
+      }
+    },
   },
   plugins: [],
 }
